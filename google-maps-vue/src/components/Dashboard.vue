de35b7978590641c203c4c21ee322121801c8356
Adam Freymiller <ADAM.FREYMILLER@morningstar.com>
2020-03-16 19:56:24 -0500

    proof of concept of map loading

--- google-maps-vue/src/components/Dashboard.vue
+++ google-maps-vue/src/components/Dashboard.vue
@@ -1,7 +1,20 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-  </div>
+  <!--<div id="map"></div>-->
+  <GmapMap
+  :center="{lat:10, lng:10}"
+  :zoom="7"
+  map-type-id="terrain"
+  style="width: 500px; height: 300px"
+>
+  <GmapMarker
+    :key="index"
+    v-for="(m, index) in markers"
+    :position="m.position"
+    :clickable="true"
+    :draggable="true"
+    @click="center=m.position"
+  />
+</GmapMap>
 </template>
 
 <script>
@@ -15,6 +28,10 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+#map {
+        height: 100%;
+      }
+
 h3 {
   margin: 40px 0 0;
 }
